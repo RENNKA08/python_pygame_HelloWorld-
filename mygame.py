25d693e3e4082aa035baa13fcdf6a8cc832badb8
RENNKA08 <rennka08.31@gmail.com>
2018-11-27 20:36:21 +0900

    make load image

--- mygame.py
+++ mygame.py
@@ -15,36 +15,45 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 # タイトルバーの文字列をセット
 pygame.display.set_caption(u"HelloWorld!")
 
-#フォントの作成
+# フォントの作成
 sysfont = pygame.font.SysFont(None, 100)
 sysfont1 = pygame.font.SysFont(None, 30)
 
-#テキストを描画したSurfaceを作成
+# テキストを描画したSurfaceを作成
 title = sysfont.render("HelloWorld!", True, (0, 0, 0))
 start = sysfont1.render("TAP TO SPACE", True, (0, 0, 0))
 
 # イメージを用意
 playerImg = pygame.image.load("akari_dot2.png").convert_alpha()
 playerImg_rect = playerImg.get_rect()
-print(playerImg_rect.height)
+# print(playerImg_rect.height)
 
-#初期化
+# 初期化
 X_std = 100
-x_load = 640
+Y_std = 240
+x_load = []
+y_load = []
+i = 0
+while True:
+    x_load.append(i)
+    y_load.append(Y_std)
+    i += 1
+    if i == 641:
+        break
+# print(x_load)
 x = X_std
 x_move = 20  # 要変更
-Y_std = 240
-y_load = Y_std
 y = Y_std
 y_move = playerImg_rect.height
 load_random = 0
+new_field = Y_std
 
 # ゲームループ
 while True:
-    screen.fill((255,255,255))   # 画面を白色で塗りつぶす
+    screen.fill((255, 255, 255))   # 画面を白色で塗りつぶす
     flag = False
 
-    #テキストを描画する
+    # テキストを描画する
     screen.blit(title, (125, 180))
     screen.blit(start, (240, 275))
 
@@ -52,7 +61,8 @@ while True:
     # イベント処理
 
     for event in pygame.event.get():
-        if event.type == QUIT: sys.exit()
+        if event.type == QUIT:
+            sys.exit()
         if event.type == KEYDOWN:  # 終了イベント
             if event.key == K_ESCAPE:
                 sys.exit()
@@ -68,61 +78,37 @@ while True:
     screen.fill((255, 255, 255))
 
     # 道の生成
-    """
     load_random = random.uniform(0, 1000)
-    if y_load == Y_std:
-        if load_random < 50:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-                if y_load < Y_std + y_move:
-                    y_load += 1
-        else:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-    if y_load == Y_std + y_move:
-        if load_random < 50:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-                if y_load < Y_std + y_move + y_move:
-                    y_load += 1
-        else:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-        if load_random > 950:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-                if y_load > Y_std:
-                    y_load -= 1
-        else:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-    if y_load == Y_std + y_move + y_move:
-        if load_random > 950:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-                if y_load > Y_std + y_move:
-                    y_load -= 1
-        else:
-            while x_load > 0:
-                pygame.draw.line(screen, (0, 0, 0), (x_load, y_load), (x_load - 1, y_load), 1)
-                x_load -= 1
-    """
+    i = 0
+    while True:
+        y_load[i] = y_load[i+1]
+        i += 1
+        if i == 640:
+            break
+    if new_field == Y_std:
+        if load_random < 1:
+            new_field = Y_std + y_move
+    if new_field == Y_std + y_move:
+        if load_random > 1:
+            if load_random < 2:
+                new_field = Y_std + y_move + y_move
+        if load_random < 999:
+            if load_random > 998:
+                new_field = Y_std
+    if new_field == Y_std + y_move + y_move:
+        if load_random > 999:
+            new_field = Y_std + y_move
+    y_load[640] = new_field
+    i = 640
+    while True:
+        pygame.draw.line(screen, (0, 0, 0), (x_load[i], y_load[i]), (x_load[i] - 1, y_load[i]), 1)
+        i -= 1
+        if i == -1:
+            break
 
     screen.blit(playerImg, (x, y))
 
     pygame.display.update()
-
-    x_load = 640
-
-    if x_load == 0:
-        x_load = 640
 
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -135,7 +121,8 @@ while True:
                     x -= x_move
                     y += y_move
 
-        if event.type == QUIT: sys.exit()
+        if event.type == QUIT:
+            sys.exit()
         if event.type == KEYDOWN:  # 終了イベント
             if event.key == K_ESCAPE:
                 sys.exit()
